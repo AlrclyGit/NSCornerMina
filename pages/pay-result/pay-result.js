b35d8eb4f6bcbc6b6572fba1b6fd6d2b6b0e76ac
AlrclySuzuki <Alrcly@foxmail.com>
2021-03-02 15:31:08 +0800

    支付后落地页添加注释

--- pages/pay-result/pay-result.js
+++ pages/pay-result/pay-result.js
@@ -1,33 +1,29 @@
 Page({
 
-    /**
-     * 
-     */
     data: {
 
     },
 
     /**
-     *
+     * 页面加载-生命周期函数
      */
     onLoad: function (options) {
         this.setData({
-            payResult: options.flag,
-            id: options.id,
-            from: options.from
+            payResult: options.flag, // 支付状态
+            id: options.id, // 订单ID
+            from: options.from // 数据来源
         });
     },
 
     /**
-     * 
+     * 查看订单
      */
     viewOrder: function () {
-        if (this.data.from == 'my') {
+        if (this.data.from == 'my') { // 来源为个人页，跳转到订单页
             wx.redirectTo({
                 url: '../order/order?from=order&id=' + this.data.id
             });
-        } else {
-            //返回上一级
+        } else { // 其他来源则，返回上一级
             wx.navigateBack({
                 delta: 1
             })
